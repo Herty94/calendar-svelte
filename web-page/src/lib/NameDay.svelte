ba98f2175db4e29a196aea348e27a28184b88dc2
dh843qp <david.hertneky@student.tuke.sk>
2024-12-17 20:54:04 +0100

    init

---
+++ web-page/src/lib/NameDay.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+  let namedayData:string;
+
+  onMount(async()=>{
+    try{
+      const response = await fetch('https://nameday.abalin.net/api/V1/today?country=sk');
+      if (!response.ok) {
+        throw new Error('Failed to fetch nameday data');
+      }
+      const data = await response.json();
+      namedayData = data.nameday?.sk;
+    }catch(e){
+      console.log(e)
+    }
+  })
+  
+</script>
+
+<div>
+  {#if namedayData}
+  <h2>{namedayData}</h2>
+{:else}
+  <p>Error</p>
+{/if}
+</div>
