c3c75a384e507e4709a9b1f3335846adccb224cc
dh843qp <david.hertneky@student.tuke.sk>
2024-12-29 11:46:33 +0100

    contrast up

--- web-page/src/lib/VerseOfDay.svelte
+++ web-page/src/lib/VerseOfDay.svelte
@@ -7,3 +7,8 @@
   <img width="100%" height="auto" src='https://www.bible.com/_next/image?url=https%3A%2F%2Fimageproxy.youversionapi.com%2F640x640%2Fhttps%3A%2F%2Fs3.amazonaws.com%2Fstatic-youversionapi-com%2Fimages%2Fbase%2F70652%2F1280x1280.jpg%20&w=640&q=75' alt="verse of the day"/>
 </div>
 
+<style>
+  img {
+    filter: contrast(1.8)
+  }
+</style>
